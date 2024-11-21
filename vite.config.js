ab5fbea4e9e2fd94393aef3cca05e81581444902
TEGAR ARSYADANI <tegararsya0117@gmail.com>
2024-11-22 02:13:28 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'vendor/filament/filament/resources/css/theme.css'],
             refresh: true,
         }),
     ],
